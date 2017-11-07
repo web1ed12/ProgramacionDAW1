b716505433c07915bc4d9932e94279e752245345
web1ed12 <victorcampezgz@gmail.com>
2017-11-07 20:16:33 +0100

    primer comit

---
+++ Ejercicio10vectores/src/Ejercicio10vectores.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Ejercicio10vectores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		String frase = "";
+		final int MAX = 60;
+		do{
+			System.out.println("Frase hasta max 60 caracteres");
+			frase = teclado.nextLine();
+		}while(frase.length() > MAX);
+		frase = frase.toUpperCase();	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
